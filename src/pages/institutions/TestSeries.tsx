8bcdfb85a7b1ab47a435c42adbc5094978b29057
krishna <tiksgupta20@gmail.com>
2026-05-28 22:39:08 +0530

    build test series institution page

--- src/pages/institutions/TestSeries.tsx
+++ src/pages/institutions/TestSeries.tsx
@@ -1,5 +1,19 @@
+import TestSeriesHero from "../../sections/test-series/TestSeriesHero";
+import TestSeriesOverview from "../../sections/test-series/TestSeriesOverview";
+import EvaluationSection from "../../sections/test-series/EvaluationSection";
+import BenefitsSection from "../../sections/test-series/BenefitSection";
+import TestSeriesCTA from "../../sections/test-series/TestSeriesCTA";
+
 const TestSeries = () => {
-  return <div>Test Series</div>;
+  return (
+    <>
+      <TestSeriesHero />
+      <TestSeriesOverview />
+      <EvaluationSection />
+      <BenefitsSection />
+      <TestSeriesCTA />
+    </>
+  );
 };
 
 export default TestSeries;
